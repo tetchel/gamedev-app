102f88fd00b0c542924ae7c37c552efff6c1ea55
Tim Etchells <tetchel@gmail.com>
2017-10-10 15:02:43 -0400

    Huge update to player movement

--- Assets/script/MyThirdPersonCamera.cs
+++ Assets/script/MyThirdPersonCamera.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MyThirdPersonCamera : MonoBehaviour {
-
-    public GameObject player;
+    
+    public GameObject target;
 
     private Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
-        offset = transform.position - player.transform.position;
-	}
-	
-	// LateUpdate is called once per frame after Update() is complete - set camera position AFTER player has moved
-	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-        //Debug.Log("Player " + player.transform.position + " Transform " + )
-	}
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.transform.position;
+
+        // Since we're only using the mouse to rotate, we don't want a cursor, 
+        // and we want to confine the cursor to the window
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // LateUpdate is called once per frame after Update() is complete - set camera position AFTER target has moved
+    void LateUpdate () {
+        float mouseX = Input.GetAxis("Mouse X");
+        //float mouseY = Input.GetAxis("Mouse Y");
+
+        // Move the camera based on mouse movement, then look at the player
+        // The mouseX scalar must (?) match the player's turn speed 
+        offset = Quaternion.AngleAxis(mouseX * 4, Vector3.up) * offset;
+        transform.position = target.transform.position + offset;
+
+        Vector3 cameraLook = target.transform.position;
+        cameraLook.y = cameraLook.y + 2;
+        transform.LookAt(cameraLook);
+    }
+
 }
--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -4,28 +4,68 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private Rigidbody rigidBody;
+    private Rigidbody rb;
+    private Animator animator;
 
-    public float speed;
+    private const int FWD_SPEED = 5;
+    private const int BWD_SPEED = 3;
+    private const int HORZ_SPEED = 3;
+    private const int TURN_SPEED = 4;
 
-	// Use this for initialization
-	void Start () {
-        rigidBody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called before each frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
 
-    // FixedUpdate should be used instead of Update when dealing with Rigidbody
-    void FixedUpdate() {
-        // should these be called here or in Update() ?
+    // LateUpdate so that input will be in by the time we animate
+    void LateUpdate() {
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
+        
+        // Rotate the player based on mouse movement
+        rb.transform.Rotate(0, Input.GetAxis("Mouse X") * TURN_SPEED, 0);
 
-        // Determine player heading based on inputs
-        Vector3 vel = new Vector3(horiz, 0, vert);
-        rigidBody.velocity = vel * speed;
+        animate(horiz, vert);
+    }
+     
+    private void animate(float horiz, float vert) {
+        // Animator uses the exact same logic as below to pick animation direction
+        animator.SetFloat("vert", vert);
+        animator.SetFloat("horiz", horiz);
+
+        // Vertical movement precedes horizontal - will add blending later?
+        // forward 
+        if(vert > 0.1) {
+            move(rb.transform.forward, FWD_SPEED);
+        }
+        // backward
+        else if(vert < -0.1) {
+            move(-rb.transform.forward, BWD_SPEED);
+        }
+        // right
+        else if(horiz > 0.1) {
+            move(rb.transform.right, HORZ_SPEED);
+        }
+        // left
+        else if(horiz < -0.1) {
+            move(-rb.transform.right, HORZ_SPEED);
+        }
+
+        /*
+        float sprint = isSprinting() ? 0.2f : 0.0f;
+
+        animator.SetFloat("sprint", sprint);
+
+        Debug.Log("turn" + horiz + " walk" + vert + " sprint" + sprint);
+        Debug.Log(animator.gameObject.name);*/
+    }
+
+    private void move(Vector3 direction, int speed) {
+        rb.MovePosition(rb.transform.position + (direction * Time.deltaTime * speed));
+    }
+
+    private bool isSprinting() {
+        return Input.GetButton("Fire1");
     }
 }
