4568e8854519e84e6b5e47197dd17b8d91b9daa5
Tim Etchells <tetchel@gmail.com>
2017-10-19 23:25:43 -0400

    Good progress on camera and aiming

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour {
 
-    private Rigidbody rb;
     private Animator animator;
+    private Transform playerTransform;
+    private CharacterController controller;
 
     private const int FWD_SPEED = 5;
     private const int BWD_SPEED = 3;
@@ -13,44 +16,61 @@ public class PlayerMovement : MonoBehaviour {
 
     private const int TURN_SPEED = 4;
 
+    private const int GRAVITY = 10;
+
     // Use this for initialization
     void Start() {
         animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
     }
 
-    void FixedUpdate() {
+    void LateUpdate() {
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
         
         // Rotate the player based on mouse movement
-        rb.transform.Rotate(0, Input.GetAxis("Mouse X") * Constants.TURN_SPEED * Time.deltaTime, 0);
+        playerTransform.Rotate(0, Input.GetAxis("Mouse X") * Constants.TURN_SPEED * Time.deltaTime, 0);
 
         animate(horiz, vert);
     }
      
     private void animate(float horiz, float vert) {
         // Animator uses the exact same logic as below to pick animation direction
+
+        // Vertical movement precedes horizontal
+        // For now if player holds l/r then u/d it will result in weirdness - decide how to handle blending
         animator.SetFloat("vert", vert);
+        if(vert != 0) {
+            horiz = 0;
+        }
         animator.SetFloat("horiz", horiz);
 
-        // Vertical movement precedes horizontal - will add blending later?
+        Vector3 inputDir = Vector3.zero;
+        int speed = 0;
         // forward 
         if(vert > 0.1) {
-            move(rb.transform.forward, FWD_SPEED);
+            inputDir = playerTransform.forward;
+            speed = FWD_SPEED;
         }
         // backward
         else if(vert < -0.1) {
-            move(-rb.transform.forward, BWD_SPEED);
+            inputDir = -playerTransform.forward;
+            speed = BWD_SPEED;
         }
         // right
         else if(horiz > 0.1) {
-            move(rb.transform.right, HORZ_SPEED);
+            inputDir = playerTransform.right;
+            speed = HORZ_SPEED;
         }
         // left
         else if(horiz < -0.1) {
-            move(-rb.transform.right, HORZ_SPEED);
+            inputDir = -playerTransform.right;
+            speed = HORZ_SPEED;
         }
+        
+        // Always move - This will apply gravity even if there is no input.
+        move(inputDir, speed);
 
         /*
         float sprint = isSprinting() ? 0.2f : 0.0f;
@@ -61,10 +81,12 @@ public class PlayerMovement : MonoBehaviour {
         Debug.Log(animator.gameObject.name);*/
     }
 
+    // Apply gravity and move the player in the given direction at the given speed
     private void move(Vector3 direction, int speed) {
-        rb.MovePosition(rb.transform.position + (direction * Time.deltaTime * speed));
-        //rb.velocity = direction * Time.deltaTime * speed;
-        //rb.AddForce(direction * speed);
+        direction.y -= GRAVITY * Time.deltaTime;
+        direction.x *= speed;
+        direction.z *= speed;
+        controller.Move(direction * Time.deltaTime);
     }
 
     private bool isSprinting() {
---
+++ Assets/script/ThirdPersonShooterCam.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonShooterCam : MonoBehaviour {
+    
+    public Transform player;
+
+    // Should be removed once can rotate in Y
+    public float aimHeight = 1.6f;
+    // How high the rays leave the player from
+    public float gunHeight = 0.8f;
+
+    private Camera cam;
+
+    // Difference between player location and camera location
+    public Vector3 camToPlayerOffset;
+
+    // Where player will shoot at
+    private Vector3 playerAim;
+    
+    // In degrees, this determines how far to the right of the player the crosshair appears
+    public float crosshairOffsetH = 2f;
+    // How far above the player the crosshair appears
+    //public float crosshairOffsetV = 2f;
+
+    public Texture2D crosshairTexture;
+    private Vector2 crosshairSize;
+
+    public Rigidbody projectile;
+    // should add a Texture for the projectile
+
+    // in millis
+    public float reloadTime = 1000;
+
+    private float reloadCounter = 0;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+    
+        crosshairSize = new Vector2(crosshairTexture.width, crosshairTexture.height);
+
+        // Since we're only using the mouse to rotate, we don't want a cursor, 
+        // and we want to confine the cursor to the window
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate () {
+        cameraLookAtPlayer();
+        aimAndFire();
+    }
+
+    // Move the camera based on mouse movement, then look at the player
+    void cameraLookAtPlayer() {
+        // The mouseX scalar must (?) match the player's turn speed 
+        camToPlayerOffset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Constants.TURN_SPEED * Time.deltaTime, Vector3.up) * camToPlayerOffset;
+
+        transform.position = player.transform.position + camToPlayerOffset;
+
+        Vector3 cameraLook = player.transform.position;
+        cameraLook.y += aimHeight;
+        transform.LookAt(cameraLook);
+    }
+
+    void aimAndFire() {
+        playerAim = cam.transform.position + (cam.transform.forward * 100);
+        playerAim = Quaternion.AngleAxis(crosshairOffsetH, Vector3.up) * playerAim;
+        //Debug.Log("After: " + playerAim);
+        //playerAim = Quaternion.AngleAxis(crosshairOffsetV, Vector3.right) * playerAim;
+
+        if (reloadCounter < reloadTime) {
+            reloadCounter += Time.deltaTime * 1000;
+        }
+
+        //Debug.DrawRay(Vector3.zero, playerAim);
+        if(Input.GetButton("Fire1") && reloadCounter > reloadTime) {
+            reloadCounter = 0;
+
+            Vector3 gunPos = player.transform.position;
+            gunPos.y += gunHeight;
+            Debug.DrawRay(gunPos, playerAim, Color.black, 1, true);
+            //Debug.Log("Player at " + player.transform.position + " aiming at " + playerAim);
+
+            Quaternion aimRotation = player.transform.rotation * Quaternion.LookRotation(playerAim.normalized);
+            Rigidbody projectileInstance = Instantiate(projectile, gunPos, aimRotation);
+            projectileInstance.AddForce(projectileInstance.transform.forward * 10);
+        }
+    }
+
+    void OnGUI() {
+        // Draw crosshair
+        if (Time.timeScale != 0) {
+            Vector2 crosshairLoc = cam.WorldToScreenPoint(playerAim);
+            crosshairLoc.x -= crosshairSize.x/2;
+            crosshairLoc.y -= crosshairSize.y/2;
+
+            GUI.DrawTexture(new Rect(crosshairLoc, crosshairSize), crosshairTexture);
+        }
+    }
+}
